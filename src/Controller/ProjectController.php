b75ed18cbe3d05b4c4b46c50219242a0e155fbba
djilaliouadah <djilali.ouadah@viacesi.fr>
2021-03-24 16:40:19 +0100

    debut

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -10,11 +10,21 @@ class ProjectController extends AbstractController
 {
 
     /**
-     * @Route("/", name="project")
+     * @Route("/", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('project/home.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function connexion(): Response
+    {
+        return $this->render('project/connexion.html.twig', [
             'controller_name' => 'ProjectController',
         ]);
     }
