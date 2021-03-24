d8ee3d3fffe6ae500ebc372ccae255b16a1649c5
djilaliouadah <djilali.ouadah@viacesi.fr>
2021-03-24 17:23:46 +0100

    encore

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -38,4 +38,44 @@ class ProjectController extends AbstractController
             'controller_name' => 'ProjectController',
         ]);
     }
+
+    /**
+     * @Route("/gerer_candidature", name="gerer_candidature")
+     */
+    public function gerer_candidature(): Response
+    {
+        return $this->render('project/gerer_candidature.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    /**
+     * @Route("/gerer_entreprise", name="gerer_entreprise")
+     */
+    public function gerer_entreprise(): Response
+    {
+        return $this->render('project/gerer_entreprise.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    /**
+     * @Route("/gerer_compte", name="gerer_compte")
+     */
+    public function gerer_compte(): Response
+    {
+        return $this->render('project/gerer_compte.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    /**
+     * @Route("/gerer_offre", name="gerer_offre")
+     */
+    public function gerer_offre(): Response
+    {
+        return $this->render('project/gerer_offre.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
 }
